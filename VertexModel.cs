e21ef312ae218e3707a16f59ff5f49f19339b529
TomCP <107045601+TomCP20@users.noreply.github.com>
2024-07-09 18:18:05 +0100

    fixed vertex model bug

--- VertexModel.cs
+++ VertexModel.cs
@@ -10,9 +10,10 @@ public class VertexModel
 
     private readonly int vertexArrayObject;
 
-    public VertexModel(float[] vertices, int[] strides)
+    public VertexModel(float[] vertices, int[] AttribSizes)
     {
-        vertexCount = vertices.Length/strides.Sum();
+        int stride = AttribSizes.Sum();
+        vertexCount = vertices.Length/stride;
         vertexArrayObject = GL.GenVertexArray();
         GL.BindVertexArray(vertexArrayObject);
 
@@ -21,11 +22,11 @@ public class VertexModel
         GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
 
         int offset = 0;
-        for (int i = 0; i < strides.Length; i++)
+        for (int i = 0; i < AttribSizes.Length; i++)
         {
             GL.EnableVertexAttribArray(i);
-            GL.VertexAttribPointer(0, strides[i], VertexAttribPointerType.Float, false, sizeof(float), offset*sizeof(float));
-            offset += strides[i];
+            GL.VertexAttribPointer(0, AttribSizes[i], VertexAttribPointerType.Float, false, stride*sizeof(float), offset*sizeof(float));
+            offset += AttribSizes[i];
         }
         
 
